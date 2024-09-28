477e6e76ef4460fc8666d1abd92fe43bd5c348e2
ProgramPedro <113397385+ProgramPedro@users.noreply.github.com>
2024-09-28 18:47:48 -0400

    Add files via upload

---
+++ SHELLHACKS_2024_FX/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.shellhacks_2024_fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires jlatexmath;
+    requires com.google.gson;
+    requires java.desktop;
+    requires javafx.swing;
+
+
+    opens mainfolder to javafx.fxml;
+    exports mainfolder;
+}
