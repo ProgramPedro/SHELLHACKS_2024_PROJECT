024f82a314427177df81ec325f315245eaf0e3a7
Nelson Diaz <Diazo0605@gmail.com>
2024-09-28 14:31:51 -0400

    Google API integration for video reccomendations

--- AI/main.py
+++ AI/main.py
@@ -2,9 +2,11 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 import json
+from googleapiclient.discovery import build
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
@@ -13,9 +15,22 @@ completion = client.chat.completions.create(
         {
             "role": "user",
             "content": "Institution: Florida International Univeristy | Course: MAC2311 | Topic: Equation of tangent line using limit definition of derivative"
-        }
+        } #replace institution, course, topic with input corresponding
     ]
 )
+
+def search_videos(query):
+    youtube = build('youtube', 'v3', developerKey=os.getenv("GOOGLE_API_KEY"))
+    request = youtube.search().list(part='snippet', type='video', q=query, maxResults=3)
+    response = request.execute()
+    return response
+
+query = 'python tutorials' #replace with input topic
+results = search_videos(query)
+videos = []
+for i in range(len(results["items"])):
+    videos.append(f"https://www.youtube.com/watch?v={results['items'][i]['id']['videoId']}")
+
 
 output = completion.choices[0].message.content.split("spc")
 
@@ -23,7 +38,7 @@ print(completion.choices[0].message.content)
 
 with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
     data = json.load(file)
-    data['Response']['Explanation'] = output[0]
+    data['Response']['Explanation'] = f"{output[0]}(Extra Resources): {videos}\n\n"
     data['Response']['Question'] = output[1]
     data['Response']['Answer'] = output[-1]
     file.seek(0)
