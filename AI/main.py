9fd50c1491f7cbe03d949496f28a010fa23e56e1
Nelson Diaz <Diazo0605@gmail.com>
2024-09-28 15:02:44 -0400

    modified prompt

--- AI/main.py
+++ AI/main.py
@@ -11,7 +11,7 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation provide 1 multiple choice practice problem with no less than 4 choices THERE MUST BE A CORRECT CHOICE (DOUBLE CHECK) and explain why which is the correct answer. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' for easier formatting"},
+        {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation work out a practice problem and make the answer of the problem a choice in a 4 option multiple choice for that practice problem. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' for easier formatting"},
         {
             "role": "user",
             "content": "Institution: Florida International Univeristy | Course: MAC2311 | Topic: Equation of tangent line using limit definition of derivative"
