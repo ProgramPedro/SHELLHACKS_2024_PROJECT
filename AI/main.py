dc9efb3e809c2123d68df342781d328b6252f80e
Nelson Diaz <Diazo0605@gmail.com>
2024-09-28 13:35:23 -0400

    Changed AI prompt

--- AI/main.py
+++ AI/main.py
@@ -9,10 +9,10 @@ client = OpenAI(api_key=os.getenv("API_KEY"))
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. In the explanation provide 2-3 links to outside resources for their course. After an explanation provide 1 multiple choice practice problem with no less than 3 choices and explain why which is the correct answer. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' for easier formatting"},
+        {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation provide 1 multiple choice practice problem with no less than 4 choices THERE MUST BE A CORRECT CHOICE (DOUBLE CHECK) and explain why which is the correct answer. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' for easier formatting"},
         {
             "role": "user",
-            "content": "Institution: Florida Internation Univeristy | Course: MAC2311 | Topic: Equation of tangent line using limit definition of derivative"
+            "content": "Institution: Florida International Univeristy | Course: MAC2311 | Topic: Equation of tangent line using limit definition of derivative"
         }
     ]
 )
@@ -21,7 +21,7 @@ output = completion.choices[0].message.content.split("spc")
 
 print(completion.choices[0].message.content)
 
-with open("output.json", "r+") as file:
+with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
     data = json.load(file)
     data['Response']['Explanation'] = output[0]
     data['Response']['Question'] = output[1]
