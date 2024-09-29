48d203185511a7b386eff8a352b704e48b5a6b4e
Nelson Diaz <Diazo0605@gmail.com>
2024-09-28 20:25:49 -0400

    changed functions, input and output reading

--- AI/main.py
+++ AI/main.py
@@ -6,6 +6,11 @@ from googleapiclient.discovery import build
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+with open("SHELLHACKS_2024_PROJECT\input.json", "r+") as input:
+    data = json.load(input)
+    school = data["School"]
+    course = data["Course"]
+    topic = data['Topic']
 
 def search_videos(query):
     youtube = build('youtube', 'v3', developerKey=os.getenv("GOOGLE_API_KEY"))
@@ -14,40 +19,33 @@ def search_videos(query):
     return response
 
 def default_ai():
-    with open("SHELLHACKS_2024_PROJECT\input.json", "r+") as input:
-        data = json.load(input)
-        school = data["School"]
-        course = data["Course"]
-        topic = data['Topic']
-        completion = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation work out a practice problem and make the answer of the problem a choice in a 4 option multiple choice for that practice problem. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' and all '\' should be replaced by '$' for easier formatting. Also seperated by 'spc' put the letter of the answer alone at the bottom"},
-                {
-                    "role": "user",
-                    "content": f"Institution: {school} | Course: {course} | Topic: {topic}"
-                } 
-            ]
-        )
-        output = completion.choices[0].message.content.split("spc")
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation work out a practice problem and make the answer of the problem a choice in a 4 option multiple choice for that practice problem. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' and all '\' should be replaced by '$' for easier formatting. Also seperated by 'spc' put the letter of the answer alone at the bottom"},
+            {
+                "role": "user",
+                "content": f"Institution: {school} | Course: {course} | Topic: {topic}"
+            } 
+        ]
+    )
+    output = completion.choices[0].message.content.split("spc")
 
-        print(completion.choices[0].message.content)
-
-        results = search_videos(topic)
-        videos = []
-        for i in range(len(results["items"])):
-            videos.append((f"https://www.youtube.com/watch?v={results['items'][i]['id']['videoId']}", results["items"][i]['snippet']['title']))
+    results = search_videos(topic)
+    videos = []
+    for i in range(len(results["items"])):
+        videos.append((f"https://www.youtube.com/watch?v={results['items'][i]['id']['videoId']}", results["items"][i]['snippet']['title']))
 
 
-        with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
-            data = json.load(file)
-            data['Response']['Explanation'] = f"{output[0]}(Extra Resources): {videos[0][0]} ({videos[0][-1]})\n{videos[1][0]} ({videos[1][-1]})\n{videos[-1][0]} ({videos[-1][-1]})\n\n"
-            data['Response']['Question'] = output[1]
-            data['Response']['Answer'] = output[2]
-            data['Response']['correctAnswer'] = output[-1]
-            file.seek(0)
-            file.truncate(0)
-            file.write(json.dumps(data))
+    with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
+        data = json.load(file)
+        data['Response']['Explanation'] = f"{output[0]}(Extra Resources): {videos[0][0]} ({videos[0][-1]})\n{videos[1][0]} ({videos[1][-1]})\n{videos[-1][0]} ({videos[-1][-1]})\n\n"
+        data['Response']['Question'] = output[1]
+        data['Response']['Answer'] = output[2]
+        data['Response']['correctAnswer'] = output[-1]
+        file.seek(0)
+        file.truncate(0)
+        file.write(json.dumps(data))
     return output[0]
 
 def followup_ai():
@@ -64,10 +62,9 @@ def followup_ai():
                 } 
             ]
         )
-        output = completion.choices[0].message.content.split("spc")
+        output = completion.choices[0].message.content
 
         with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
-            file.truncate()
-            file.write(json.dumps(data))
-    return output[0]
-
+            data = json.load(file)
+            data["output"] = output
+    return output
