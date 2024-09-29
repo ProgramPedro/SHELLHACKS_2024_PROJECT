87b389061bd5dbd66327f950a25fe810dd4ca0dc
Nelson Diaz <Diazo0605@gmail.com>
2024-09-28 20:13:22 -0400

    functions added ai stuff

--- AI/main.py
+++ AI/main.py
@@ -7,44 +7,67 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-
-with open("SHELLHACKS_2024_PROJECT\input.json", "r+") as input:
-    data = json.load(input)
-    school = data["School"]
-    course = data["Course"]
-    topic = data['Topic']
-    completion = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation work out a practice problem and make the answer of the problem a choice in a 4 option multiple choice for that practice problem. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' and all '\' should be replaced by '$' for easier formatting."},
-            {
-                "role": "user",
-                "content": f"Institution: {school} | Course: {course} | Topic: {topic}"
-            } 
-        ]
-    )
-
 def search_videos(query):
     youtube = build('youtube', 'v3', developerKey=os.getenv("GOOGLE_API_KEY"))
     request = youtube.search().list(part='snippet', type='video', q=query, maxResults=3)
     response = request.execute()
     return response
 
-results = search_videos(topic)
-videos = []
-for i in range(len(results["items"])):
-    videos.append((f"https://www.youtube.com/watch?v={results['items'][i]['id']['videoId']}", results["items"][i]['snippet']['title']))
+def default_ai():
+    with open("SHELLHACKS_2024_PROJECT\input.json", "r+") as input:
+        data = json.load(input)
+        school = data["School"]
+        course = data["Course"]
+        topic = data['Topic']
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it please refer to the institution and course they provided. After an explanation work out a practice problem and make the answer of the problem a choice in a 4 option multiple choice for that practice problem. The explanation, multiple choice question, and the answer explanation should be seperated by 'spc' and all '\' should be replaced by '$' for easier formatting. Also seperated by 'spc' put the letter of the answer alone at the bottom"},
+                {
+                    "role": "user",
+                    "content": f"Institution: {school} | Course: {course} | Topic: {topic}"
+                } 
+            ]
+        )
+        output = completion.choices[0].message.content.split("spc")
+
+        print(completion.choices[0].message.content)
+
+        results = search_videos(topic)
+        videos = []
+        for i in range(len(results["items"])):
+            videos.append((f"https://www.youtube.com/watch?v={results['items'][i]['id']['videoId']}", results["items"][i]['snippet']['title']))
 
 
-output = completion.choices[0].message.content.split("spc")
+        with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
+            data = json.load(file)
+            data['Response']['Explanation'] = f"{output[0]}(Extra Resources): {videos[0][0]} ({videos[0][-1]})\n{videos[1][0]} ({videos[1][-1]})\n{videos[-1][0]} ({videos[-1][-1]})\n\n"
+            data['Response']['Question'] = output[1]
+            data['Response']['Answer'] = output[2]
+            data['Response']['correctAnswer'] = output[-1]
+            file.seek(0)
+            file.truncate(0)
+            file.write(json.dumps(data))
+    return output[0]
 
-print(completion.choices[0].message.content)
+def followup_ai():
+    with open("SHELLHACKS_2024_PROJECT\input.json", "r+") as input:
+        data = json.load(input)
+        input = data["Input"]
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "system", "content": "You are a professor who is aiding students in their desired topics in order for them to better understand it. You are here to help them interpret your past responses to their questions and follow up on any more questions they have make sure that all '\' in your response should be replaced by '$' for easier formatting."},
+                {
+                    "role": "user",
+                    "content": f"You responded with {default_ai()}, they are asking now '{input}'"
+                } 
+            ]
+        )
+        output = completion.choices[0].message.content.split("spc")
 
-with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
-    data = json.load(file)
-    data['Response']['Explanation'] = f"{output[0]}(Extra Resources): {videos[0][0]} ({videos[0][-1]})\n{videos[1][0]} ({videos[1][-1]})\n{videos[-1][0]} ({videos[-1][-1]})\n\n"
-    data['Response']['Question'] = output[1]
-    data['Response']['Answer'] = output[-1]
-    file.seek(0)
-    file.truncate(0)
-    file.write(json.dumps(data))
+        with open("SHELLHACKS_2024_PROJECT\output.json", "r+") as file:
+            file.truncate()
+            file.write(json.dumps(data))
+    return output[0]
+
